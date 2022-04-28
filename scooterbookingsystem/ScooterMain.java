298884bcb1cfa82a2dc2003172c74677d0d757b1
adrialv <78487562+adrialv@users.noreply.github.com>
2022-04-28 18:36:29 -0400

    scooter main

--- scooterbookingsystem/ScooterMain.java
+++ scooterbookingsystem/ScooterMain.java
@@ -5,10 +5,6 @@ import java.time.LocalDate;
 public class ScooterMain {
     public static void main(String[] args) {
 
-        extracted();  
-    }
-
-    private static void extracted() {
-        ScooterReservation.locateSpecificScooters("Unagi", "Red", LocalDate.now(), 2);
+        ScooterReservation.locateSpecificScooters("Unagi", "Red", LocalDate.now(), 2);  
     }
 }
